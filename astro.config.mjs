3f8ee35463519778c3ea13ac49f09804f6dbc532
Nguyen Long Nhat <27698189+torn4dom4n@users.noreply.github.com>
2023-10-28 21:27:54 +0700

    feat: faster running

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,6 @@
 import { defineConfig } from 'astro/config'
 import starlight from '@astrojs/starlight'
 import tailwind from '@astrojs/tailwind'
-import mdx from '@astrojs/mdx'
 
 // https://astro.build/config
 export default defineConfig({
@@ -35,6 +34,5 @@ export default defineConfig({
     tailwind({
       applyBaseStyles: false,
     }),
-    mdx(),
   ],
 })
