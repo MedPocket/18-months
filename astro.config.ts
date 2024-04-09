6ba4538442e21bbb56e7cc92b2c670738c992ced
Nguyen Long Nhat <27698189+torn4dom4n@users.noreply.github.com>
2024-04-10 01:03:33 +0700

    new homepage

--- astro.config.ts
+++ astro.config.ts
@@ -27,6 +27,10 @@ export default defineConfig({
       },
       sidebar: [
         {
+          label: 'Trang chủ',
+          link: '/',
+        },
+        {
           label: 'Sản Khoa',
           autogenerate: {
             directory: 'san-khoa',
